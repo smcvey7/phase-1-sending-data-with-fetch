e4ba218f53d87b95f3126ec244e60af7b8af4dc9
Steven McVey <smcvey1210@gmail.com>
2022-08-21 16:58:05 -0400

    commit tree 2d50c9fd28645dd54bc90ca966473d5dc77fd5a5

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+function submitData(string1, string2) {
+  
+  const formData = {
+    name: string1,
+    email: string2,
+  }
+
+  const configurationObject = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify(formData),
+  }
+
+  const location = document.body
+
+  return fetch('http://localhost:3000/users', configurationObject)
+    .then(res => res.json())
+    .then(data => {
+      document.body.append(data.id)
+    })
+    .catch(message => document.body.append(message))
+}
